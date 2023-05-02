eb1f9b21efc5495ce009e0232150f2cdb100b6c1
Falkov Danil <dannillav@mail.ru>
2023-05-02 17:04:28 +0500

    Output count products in basket

--- src/components/layouts/header/Header.js
+++ src/components/layouts/header/Header.js
@@ -1,7 +1,19 @@
 import ShopLogo2 from '../../../assets/images/ShopLogo2.png'
 import {Link} from "react-router-dom";
+import AxiosClient from "../../../axiosClient/AxiosClient";
+import {useEffect, useState} from "react";
 
 const Header = () => {
+    const [count, setCount] = useState(0);
+
+    useEffect(() => {
+        AxiosClient.post('/CountProductInCart')
+            .then(
+                (res) => {
+                    setCount(res.data)
+                }
+            )
+    }, []);
 
     const showMenuLeft = () => {
         document.documentElement.setAttribute('show-menu-left', 'true')
@@ -45,7 +57,7 @@ const Header = () => {
                             <path d="M8 1a2.5 2.5 0 0 1 2.5 2.5V4h-5v-.5A2.5 2.5 0 0 1 8 1zm3.5 3v-.5a3.5 3.5 0 1 0-7 0V4H1v10a2 2 0 0 0 2 2h10a2 2 0 0 0 2-2V4h-3.5zM2 5h12v9a1 1 0 0 1-1 1H3a1 1 0 0 1-1-1V5z" />
                         </svg>
                         <div className="count_product">
-                            <p className='count'>1</p>
+                            <p className='count'>{count}</p>
                         </div>
                     </div>
                     <p className='label_cart'>
